ff126ad392d06ed089411fb880909ca99ef0d264
elhafi20 <bisyir_el.it-student.unibamadura.ac.id>
2025-02-04 23:18:53 +0700

    update: responsifity section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,34 +5,32 @@
       <Hero />
 
       <div class="row mt-4">
-        <div class="col-3 text-start ps-0">
+        <div class="col-12 text-center text-md-start ps-md-0">
           <h2>
-            Best
-            <strong>Foods</strong>
+            Best <strong>Foods</strong>
           </h2>
         </div>
-        
       </div>
 
-      <div class="row">
-      <div class="col-6 col-md-3 mt-4" v-for="product in products" :key="product.id">
-        <CardProduct :product="product">
-
-        </CardProduct>
+      <div class="row justify-content-center">
+        <div
+          class="col-12 col-sm-6 col-md-4 col-lg-3 mt-4"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <CardProduct :product="product" class="responsive-card" />
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import CardProduct from "@/components/CardProduct.vue";
-
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "@/firebase"; 
+import { db } from "@/firebase";
 
 export default {
   name: "HomeView",
@@ -48,26 +46,57 @@ export default {
   },
   methods: {
     async fetchProducts() {
-  try {
-    const productsRef = collection(db, "best-products"); 
-    const querySnapshot = await getDocs(productsRef);
-    this.products = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-
-    
-  } catch (error) {
-    console.error("Gagal mengambil data produk: ", error);
-  }
-},
+      try {
+        const productsRef = collection(db, "best-products");
+        const querySnapshot = await getDocs(productsRef);
+        this.products = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+      } catch (error) {
+        console.error("Gagal mengambil data produk: ", error);
+      }
+    },
   },
   mounted() {
-    this.fetchProducts(); 
+    this.fetchProducts();
   },
 };
 </script>
 
 <style>
-/* Tambahkan styling sesuai kebutuhan */
+.responsive-card img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+@media (max-width: 576px) {
+  .home h2 {
+    font-size: 1.5rem;
+    text-align: center;
+  }
+  .responsive-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+@media (max-width: 768px) {
+  .home h2 {
+    font-size: 1.8rem;
+    text-align: center;
+  }
+  .responsive-card {
+    padding: 10px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .responsive-card {
+    max-width: 90%;
+    margin: 0 auto;
+  }
+}
 </style>
